3a7c2ec47279a57ed183c78ba32ab0f9bf0af495
Alexander Popov <popov.a.connect@gmail.com>
2024-01-17 13:07:33 +0300

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { FeedbackProvider } from './context/FeedbackContext'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
